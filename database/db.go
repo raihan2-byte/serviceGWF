b2a6965de923d2ddc83ed59311cdec7c6cc4e859
raihan2-byte <raihanalfarisi4@gmail.com>
2024-07-12 00:53:40 +0700

    deploy

--- database/db.go
+++ database/db.go
@@ -2,18 +2,38 @@ package database
 
 import (
 	"log"
+	"os"
 	"payment-gwf/entity"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDb() (*gorm.DB, error) {
 
-	dsn := "root:@tcp(127.0.0.1:3306)/gwf-apps?charset=utf8mb4&parseTime=True&loc=Local"
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); exists == false {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("error loading .env file:", err)
+		}
+	}
+
+	dbUsername := os.Getenv("MYSQLUSER")
+	dbPassword := os.Getenv("MYSQLPASSWORD")
+	dbHost := os.Getenv("MYSQLHOST")
+	dbPort := os.Getenv("MYSQLPORT")
+	dbName := os.Getenv("MYSQLDATABASE")
+
+	// dsn := "root:@tcp(127.0.0.1:3306)/gwf-apps?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatal("DB Connection Error")
+	// }
+
+	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("DB Connection Error")
+		log.Fatal("DB Connection Error", err)
 	}
 
 	// Auto Migration
